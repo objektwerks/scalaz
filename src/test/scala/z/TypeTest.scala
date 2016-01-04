2e722f87a05af9c9c94b80cebe6729a0cd1044ea
objektwerks <objektwerks@gmail.com>
2016-01-04 12:19:15 -0500

    type test works correctly

--- src/test/scala/z/TypeTest.scala
+++ src/test/scala/z/TypeTest.scala
@@ -7,18 +7,12 @@ trait Printable[A] {
 }
 
 object PrintDefaults {
-  implicit val stringPrintable = new Printable[String] {
+  implicit val printableString = new Printable[String] {
     def format(value: String): String = value.toString
   }
 
-  implicit val intPrintable = new Printable[Int] {
+  implicit val printableInt = new Printable[Int] {
     def format(value: Int): String = value.toString
-  }
-
-  implicit val dogPrintable = new Printable[Dog] {
-    def format(value: Dog): String = {
-      s"Name: ${value.name}, Age: ${value.age}, Color: ${value.color}."
-    }
   }
 }
 
@@ -28,7 +22,7 @@ object Print {
   }
 
   def print[A] (value: A) (implicit printer: Printable[A]): Unit = {
-    printer.format(value)
+    println(printer.format(value))
   }
 }
 
@@ -39,20 +33,26 @@ object PrintSyntax {
     }
 
     def print(implicit printer: Printable[A]): Unit = {
-      println(printer.format(value))
+      printer.format(value)
     }
   }
 }
 
 case class Dog(name: String, age: Int, color: String)
 
+object Dog {
+  implicit val printableDog = new Printable[Dog] {
+    def format(dog: Dog): String = {
+      s"Name: ${dog.name}, Age: ${dog.age}, Color: ${dog.color}."
+    }
+  }
+}
 class TypeTest extends FunSuite {
   test("printable") {
-    import PrintDefaults._
     import PrintSyntax._
 
-    val dog = new Dog("whilhelm", 33, "white")
+    val dog = Dog("whilhelm", 33, "white")
     assert(dog.format == "Name: whilhelm, Age: 33, Color: white.")
-    dog.print
+    Print.print(dog)
   }
 }
