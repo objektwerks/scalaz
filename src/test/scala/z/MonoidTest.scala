eda6fa82ae8188b1a182b3bdcb11236408f38f49
objektwerks <objektwerks@gmail.com>
2016-01-05 09:52:31 -0500

    enhanced monoid test

--- src/test/scala/z/MonoidTest.scala
+++ src/test/scala/z/MonoidTest.scala
@@ -9,5 +9,8 @@ class MonoidTest extends FunSuite {
   test("monoid") {
     3 assert_=== Monoid[Int].append(1, 2)
     3 assert_=== 1 |+| 2 |+| mzero[Int]
+
+    List(1, 2) assert_=== Monoid[List[Int]].append(List(1), List(2))
+    List(1, 2) assert_=== List(1) |+| List(2) |+| mzero[List[Int]]
   }
 }
