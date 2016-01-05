7c39516db6e45122bc33b8348b3a85c6b913852e
objektwerks <objektwerks@gmail.com>
2016-01-05 13:48:58 -0500

    added custom monoid

--- src/test/scala/z/MonoidTest.scala
+++ src/test/scala/z/MonoidTest.scala
@@ -5,6 +5,10 @@ import org.scalatest.FunSuite
 import scalaz.Scalaz._
 import scalaz._
 
+case class Data(n: Int) {
+  def +(data: Data): Data = Data(this.n + data.n)
+}
+
 class MonoidTest extends FunSuite {
   test("monoid") {
     3 assert_=== Monoid[Int].append(1, 2)
@@ -15,5 +19,10 @@ class MonoidTest extends FunSuite {
 
     List(1, 2) assert_=== Monoid[List[Int]].append(List(1), List(2))
     List(1, 2) assert_=== List(1) |+| List(2) |+| mzero[List[Int]]
+  }
+
+  test("custom monoid") {
+    val dataMonoid = Monoid.instance[Data](_ + _, Data(0))
+    assert(dataMonoid.append(Data(1), Data(2)) == Data(3))
   }
 }
