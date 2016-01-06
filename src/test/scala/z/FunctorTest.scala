6708fd0f24b4ae6a9729cd58d5b0d2bc0214b025
objektwerks <objektwerks@gmail.com>
2016-01-06 09:39:44 -0500

    enhanced map test

--- src/test/scala/z/FunctorTest.scala
+++ src/test/scala/z/FunctorTest.scala
@@ -7,6 +7,11 @@ import scalaz._
 
 class FunctorTest extends FunSuite {
   test("map") {
+    val f1 = (x: Int) => x + 1
+    val f2 = (y: Int) => y * 2
+    val f3 = f1 map f2
+    f3(1) assert_=== 4
+
     assert(Functor[List].map(List(1, 2, 3))(_ * 2) == List(2, 4, 6))
     assert(Functor[Option].map(Some(123))(_.toString) contains "123")
   }
