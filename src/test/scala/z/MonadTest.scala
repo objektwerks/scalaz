e3cdfabe2d3cde1bd7f409471f3832292e319719
objektwerks <objektwerks@gmail.com>
2016-01-06 15:49:03 -0500

    added bind test

--- src/test/scala/z/MonadTest.scala
+++ src/test/scala/z/MonadTest.scala
@@ -10,4 +10,9 @@ class MonadTest extends FunSuite {
     3.some assert_=== Monad[Option].point(3)
     List(3) assert_=== Monad[List].point(3)
   }
+
+  test("bind") {
+    6.some assert_=== Monad[Option].bind(Some(3))(x => Some(x * 2))
+    List(6) assert_=== Monad[List].bind(List(3))(x => List(x * 2))
+  }
 }
