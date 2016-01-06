521b134c0120c5b44cf4843e0728bf2dd71c6dd6
objektwerks <objektwerks@gmail.com>
2016-01-06 09:25:56 -0500

    added lift test

--- src/test/scala/z/FunctorTest.scala
+++ src/test/scala/z/FunctorTest.scala
@@ -7,7 +7,12 @@ import scalaz._
 
 class FunctorTest extends FunSuite {
   test("map") {
-    assert(Functor[List].map(List(1, 2, 3))(x => x * 2) == List(2, 4, 6))
+    assert(Functor[List].map(List(1, 2, 3))(_ * 2) == List(2, 4, 6))
     assert(Functor[Option].map(Some(123))(_.toString) contains "123")
+  }
+
+  test("lift") {
+    val lifted = Functor[Option].lift((x: Int) => x * 2)
+    assert(lifted(Some(3)) contains 6)
   }
 }
