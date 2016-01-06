72b7e7bf644aaec5d6b707bfed1bd555e0e96321
objektwerks <objektwerks@gmail.com>
2016-01-06 16:00:16 -0500

    added tuple and sequence tests

--- src/test/scala/z/MonadTest.scala
+++ src/test/scala/z/MonadTest.scala
@@ -12,7 +12,15 @@ class MonadTest extends FunSuite {
   }
 
   test("bind") {
-    6.some assert_=== Monad[Option].bind(Some(3))(x => Some(x * 2))
+    6.some assert_=== Monad[Option].bind(some(3))(x => some(x * 2))
     List(6) assert_=== Monad[List].bind(List(3))(x => List(x * 2))
+  }
+
+  test("tuple") {
+    Monad[Option].tuple3(some(1), some(2), some(3)) assert_=== Some((1, 2, 3))
+  }
+
+  test("sequence") {
+    Monad[Option].sequence(List(some(1), some(2), some(3))) assert_=== Some(List(1, 2, 3))
   }
 }
