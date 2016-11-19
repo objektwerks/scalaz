ca8f64bfbaf71a284dbc91580d8d902acc629009
objektwerks <objektwerks@runbox.com>
2016-11-19 14:36:22 -0500

    added fs2 stream chunks test

--- src/test/scala/z/StreamTest.scala
+++ src/test/scala/z/StreamTest.scala
@@ -1,6 +1,6 @@
 package z
 
-import fs2.{Stream, Task}
+import fs2.{Chunk, Stream, Task}
 import fs2.Stream._
 
 import scalaz.Scalaz._
@@ -13,7 +13,7 @@ class StreamTest extends FunSuite with Matchers {
     Stream(1, 2, 3).map(_ + 1).toList shouldBe List(2, 3, 4)
     Stream(1, 2, 3).filter(_ % 2 == 0).toList shouldBe List(2)
     Stream(1, 2, 3).fold(0)(_ + _).toList shouldBe List(6)
-    Stream(none, 2.some, 3.some).collect{ case Some(x) => x}.toList shouldBe List(2, 3)
+    Stream(none, 2.some, 3.some).collect { case Some(x) => x}.toList shouldBe List(2, 3)
     Stream.range(1, 4).intersperse(0).toList shouldBe List(1, 0, 2, 0, 3)
     Stream(1, 2, 3).flatMap(x => Stream(x, x)).toList shouldBe List(1, 1, 2, 2, 3, 3)
     Stream(1,2,3).repeat.take(9).toList shouldBe List(1, 2, 3, 1, 2, 3, 1, 2, 3)
@@ -21,5 +21,9 @@ class StreamTest extends FunSuite with Matchers {
 
   test("effects") {
     Stream.eval(Task.delay { 1 + 2 }).runLog.unsafeRun() shouldBe Vector(3)
+  }
+
+  test("chunks") {
+    Stream.chunk(Chunk.doubles(Array(1.0, 2.0, 3.0))).mapChunks { d => d.toDoubles }.fold(0.0)(_ + _).runLog.right.getOrElse(Vector(-1.0)) shouldBe Vector(6.0)
   }
 }
