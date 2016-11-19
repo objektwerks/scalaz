d0084d9848326abbc59ad6e0151426c91cd14f3b
objektwerks <objektwerks@runbox.com>
2016-11-19 13:15:23 -0500

    added fs2 stream test, a work in progress

---
+++ src/test/scala/z/StreamTest.scala
@@ -0,0 +1,19 @@
+package z
+
+import fs2.Stream
+import fs2.Stream._
+import org.scalatest.{FunSuite, Matchers}
+
+class StreamTest extends FunSuite with Matchers {
+  test("pure") {
+    Stream(1, 2, 3).toList shouldBe List(1, 2, 3)
+    (Stream(1, 2, 3) ++ Stream(4, 5, 6)).toList shouldBe List(1, 2, 3, 4, 5, 6)
+    Stream(1, 2, 3).map(_ + 1).toList shouldBe List(2, 3, 4)
+    Stream(1, 2, 3).filter(_ % 2 == 0).toList shouldBe List(2)
+    Stream(1, 2, 3).fold(0)(_ + _).toList shouldBe List(6)
+  }
+
+  test("effectual") {
+
+  }
+}
