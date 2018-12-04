85f45863130b0c0cb6cd848acd219eddbe2960f0
objektwerks <objektwerks@runbox.com>
2018-12-04 10:19:10 -0500

    edited zio test

--- src/test/scala/zio/ZioTest.scala
+++ src/test/scala/zio/ZioTest.scala
@@ -3,9 +3,13 @@ package zio
 import org.scalatest.{FunSuite, Matchers}
 import scalaz.zio._
 
-class ZioTest extends FunSuite with Matchers {
+import scala.io.Source
+
+class ZioTest extends FunSuite with Matchers with RTS {
   test("purity") {
-    IO.point("point").map(s => s shouldBe "point")
-    IO.now("now").map(s => s shouldBe "now")
+    unsafeRun( IO.point("point").map(s => s shouldBe "point") )
+    unsafeRun( IO.now("now").map(s => s shouldBe "now") )
+    unsafeRun( IO.sync(System.nanoTime()).map(t => assert(t > 0)) )
+    unsafeRun( IO.syncException(Source.fromFile("build.sbt").getLines.mkString).map(s => assert(s.nonEmpty)) )
   }
 }
