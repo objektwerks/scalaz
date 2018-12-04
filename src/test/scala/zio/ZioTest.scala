59bb3ac0b63c0f4e31df838642c44474fa711d1f
objektwerks <objektwerks@runbox.com>
2018-12-04 13:19:24 -0500

    edited zio test

--- src/test/scala/zio/ZioTest.scala
+++ src/test/scala/zio/ZioTest.scala
@@ -4,12 +4,17 @@ import org.scalatest.{FunSuite, Matchers}
 import scalaz.zio._
 
 import scala.io.Source
+import scala.util.Try
 
 class ZioTest extends FunSuite with Matchers with RTS {
+  def fileToString(file: String): IO[Throwable, String] = IO.fromTry( Try( Source.fromFile(file).getLines.mkString ) )
+
   test("purity") {
     unsafeRun( IO.point("point").map(s => s shouldBe "point") )
     unsafeRun( IO.now("now").map(s => s shouldBe "now") )
-    unsafeRun( IO.sync(System.nanoTime()).map(t => assert(t > 0)) )
-    unsafeRun( IO.syncException(Source.fromFile("build.sbt").getLines.mkString).map(s => assert(s.nonEmpty)) )
+  }
+
+  test("failure") {
+    unsafeRun( fileToString("build.bat").catchAll(_ => fileToString("build.sbt")) )
   }
 }
