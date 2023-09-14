37ff85ddb8b4f39677fe265a0a1b2636cc7443a5
objektwerks <objektwerks@runbox.com>
2023-09-14 10:12:33 -0400

    reformatted test

--- src/test/scala/z/TypeTest.scala
+++ src/test/scala/z/TypeTest.scala
@@ -2,11 +2,10 @@ package z
 
 import org.scalatest.funsuite.AnyFunSuite
 
-trait Printable[A] {
+trait Printable[A]:
   def format(value: A): String
-}
 
-object PrintDefaults {
+object PrintDefaults:
   given Printable[String] = new Printable[String] {
     def format(value: String): String = value.toString
   }
@@ -14,40 +13,26 @@ object PrintDefaults {
   given Printable[Int] = new Printable[Int] {
     def format(value: Int): String = value.toString
   }
-}
 
-object Print {
-  def format[A] (value: A) (implicit formatter: Printable[A]): String = {
-    formatter.format(value)
-  }
+object Print:
+  def format[A] (value: A) (implicit formatter: Printable[A]): String = formatter.format(value)
 
-  def print[A] (value: A) (implicit printer: Printable[A]): Unit = {
-    println(printer.format(value))
-  }
-}
+  def print[A] (value: A) (implicit printer: Printable[A]): Unit = println(printer.format(value))
 
-object PrintSyntax {
-  implicit class PrintOps[A](value: A) {
-    def format(implicit printable: Printable[A]): String = {
-      printable.format(value)
-    }
+object PrintSyntax:
+  implicit class PrintOps[A](value: A):
+    def format(implicit printable: Printable[A]): String = printable.format(value)
 
-    def print(implicit printable: Printable[A]): Unit = {
-      println(printable.format(value))
-    }
-  }
-}
+    def print(implicit printable: Printable[A]): Unit = println(printable.format(value))
 
-case class Dog(name: String, age: Int, color: String)
+final case class Dog(name: String, age: Int, color: String)
 
-object Dog {
+object Dog:
   given Printable[Dog] = new Printable[Dog] {
-    def format(dog: Dog): String = {
-      s"Name: ${dog.name}, Age: ${dog.age}, Color: ${dog.color}."
-    }
+    def format(dog: Dog): String = s"Name: ${dog.name}, Age: ${dog.age}, Color: ${dog.color}."
   }
-}
-class TypeTest extends AnyFunSuite {
+
+final class TypeTest extends AnyFunSuite:
   test("printable") {
     import PrintSyntax.*
 
@@ -56,4 +41,3 @@ class TypeTest extends AnyFunSuite {
     Print.print(dog)
     dog.print
   }
-}
