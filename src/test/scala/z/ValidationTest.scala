4e488d0cce24f7b1a0573f163f8ecff4559aa061
objektwerks <objektwerks@runbox.com>
2023-09-14 10:14:12 -0400

    reformatted test

--- src/test/scala/z/ValidationTest.scala
+++ src/test/scala/z/ValidationTest.scala
@@ -2,28 +2,26 @@ package z
 
 import org.scalatest.funsuite.AnyFunSuite
 
-import scalaz.Scalaz._
-import scalaz._
+import scalaz.*
+import scalaz.Scalaz.*
 
-case class Profile(name: String, email: String) {
-  private def validateName(name: String): Validation[String, String] = {
-    if (name.isEmpty) "Please, enter your name.".failure else name.success
-  }
+final case class Profile(name: String, email: String):
+  private def validateName(name: String): Validation[String, String] =
+    if name.isEmpty then "Please, enter your name.".failure
+    else name.success
 
-  private def validateEmail(email: String): Validation[String, String] = {
-    if (email.isEmpty) "Please, enter your email.".failure else email.success
-  }
+  private def validateEmail(email: String): Validation[String, String] =
+    if email.isEmpty then "Please, enter your email.".failure
+    else email.success
 
-  def isValid: Boolean = {
+  def isValid: Boolean =
     val validation = (validateName(name).toValidationNel |@| validateEmail(email).toValidationNel) { (_ , _) }
     validation match {
       case Success(_) => true
       case Failure(_) => false
     }
-  }
-}
 
-class ValidationTest extends AnyFunSuite {
+class ValidationTest extends AnyFunSuite:
   test("valid profile") {
     val profile = Profile("Barney Rebel", "barney.rebel@gmail.com")
     assert(profile.isValid)
@@ -33,4 +31,3 @@ class ValidationTest extends AnyFunSuite {
     val profile = Profile("", "")
     assert(!profile.isValid)
   }
-}
