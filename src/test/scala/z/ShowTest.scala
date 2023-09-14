0d1fd8c2e5e62c4c835ae91b8fe08fddf0572bc8
objektwerks <objektwerks@runbox.com>
2023-09-14 10:09:42 -0400

    reformatted test

--- src/test/scala/z/ShowTest.scala
+++ src/test/scala/z/ShowTest.scala
@@ -2,12 +2,10 @@ package z
 
 import org.scalatest.funsuite.AnyFunSuite
 
+import scalaz.Scalaz.*
 
-import scalaz.Scalaz._
-
-class ShowTest extends AnyFunSuite {
+class ShowTest extends AnyFunSuite:
   test("show") {
     1.shows assert_=== "1"
     "32" + 1.show assert_=== "321"
   }
-}
