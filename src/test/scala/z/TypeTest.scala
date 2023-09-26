f219b35bac45044fd6aa6c913ab81ade456b5c19
objektwerks <objektwerks@runbox.com>
2023-09-26 10:53:10 -0400

    reformatted test

--- src/test/scala/z/TypeTest.scala
+++ src/test/scala/z/TypeTest.scala
@@ -2,17 +2,15 @@ package z
 
 import org.scalatest.funsuite.AnyFunSuite
 
-trait Printable[A]:
+sealed trait Printable[A]:
   def format(value: A): String
 
 object PrintDefaults:
-  given Printable[String] = new Printable[String] {
+  given Printable[String] = new Printable[String]:
     def format(value: String): String = value.toString
-  }
 
-  given Printable[Int] = new Printable[Int] {
+  given Printable[Int] = new Printable[Int]:
     def format(value: Int): String = value.toString
-  }
 
 object Print:
   def format[A] (value: A) (implicit formatter: Printable[A]): String = formatter.format(value)
@@ -28,16 +26,14 @@ object PrintSyntax:
 final case class Dog(name: String, age: Int, color: String)
 
 object Dog:
-  given Printable[Dog] = new Printable[Dog] {
+  given Printable[Dog] = new Printable[Dog]:
     def format(dog: Dog): String = s"Name: ${dog.name}, Age: ${dog.age}, Color: ${dog.color}."
-  }
 
 final class TypeTest extends AnyFunSuite:
-  test("printable") {
+  test("printable"):
     import PrintSyntax.*
 
     val dog = Dog("whilhelm", 33, "white")
     assert(dog.format == "Name: whilhelm, Age: 33, Color: white.")
     Print.print(dog)
     dog.print
-  }
