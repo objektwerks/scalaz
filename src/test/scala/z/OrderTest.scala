e38ca6fcfa472024be455d40f0b3795cf7194afa
objektwerks <objektwerks@runbox.com>
2023-09-26 10:51:29 -0400

    reformatted test

--- src/test/scala/z/OrderTest.scala
+++ src/test/scala/z/OrderTest.scala
@@ -4,8 +4,8 @@ import org.scalatest.funsuite.AnyFunSuite
 
 import scalaz.Scalaz.*
 
-class OrderTest extends AnyFunSuite:
-  test("order") {
+final class OrderTest extends AnyFunSuite:
+  test("order"):
     1 > 2 assert_=== false
     1 min 2 assert_=== 1
     1 max 2 assert_=== 2
@@ -13,4 +13,3 @@ class OrderTest extends AnyFunSuite:
     1 lt 2 assert_=== true
     2 gte 1 assert_=== true
     1 lte 2 assert_=== true
-  }
