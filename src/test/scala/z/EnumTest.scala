56f8840cc197eb208c781aba338e4354ad916063
objektwerks <objektwerks@runbox.com>
2023-09-26 10:39:51 -0400

    reformatted test

--- src/test/scala/z/EnumTest.scala
+++ src/test/scala/z/EnumTest.scala
@@ -6,16 +6,14 @@ import org.scalatest.matchers.should.Matchers
 import scalaz.*
 import scalaz.Scalaz.*
 
-class EnumTest extends AnyFunSuite with Matchers:
-  test("enum") {
+final class EnumTest extends AnyFunSuite with Matchers:
+  test("enum"):
     val enumeration = 'a' |-> 'z'
     enumeration.headOption.get shouldBe 'a'
     enumeration.lastOption.get shouldBe 'z'
     'o'.succ shouldBe 'p'
     'o'.pred shouldBe 'n'
-  }
 
-  test("bounded enum") {
+  test("bounded enum"):
     implicitly[Enum[Int]].min shouldBe Some(-2147483648)
     implicitly[Enum[Int]].max shouldBe Some(2147483647)
-  }
