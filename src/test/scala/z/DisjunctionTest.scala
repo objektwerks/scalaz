44aced2e9ec35430eb43097b5eafe369682b11af
objektwerks <objektwerks@runbox.com>
2023-09-26 10:39:24 -0400

    reformatted test

--- src/test/scala/z/DisjunctionTest.scala
+++ src/test/scala/z/DisjunctionTest.scala
@@ -6,13 +6,12 @@ import org.scalatest.matchers.should.Matchers
 import scalaz.*
 import scalaz.Scalaz.*
 
-class DisjunctionTest extends AnyFunSuite with Matchers:
-  test("boolean") {
+final class DisjunctionTest extends AnyFunSuite with Matchers:
+  test("boolean"):
     false /\ true shouldBe false
     false \/ true shouldBe true
-  }
 
-  test("merge") {
+  test("merge"):
     "success".right.merge shouldBe "success"
     "failure".left.merge shouldBe "failure"
 
@@ -21,30 +20,26 @@ class DisjunctionTest extends AnyFunSuite with Matchers:
 
     \/-("success").merge shouldBe "success"
     -\/("failure").merge shouldBe "failure"
-  }
 
-  test("flatmap") {
+  test("flatmap"):
     1.right[Int].flatMap(x => (x + 2).right) shouldBe \/-(3)
     val r = for {
       x <- 1.right[Int]
       y <- 2.right[Int]
     } yield x + y
     r shouldBe \/-(3)
-  }
 
-  test("get or else") {
+  test("get or else"):
     1.right[Int].getOrElse(0) shouldBe 1
     "failure".left[Int].getOrElse(0) shouldBe 0
 
     1.right[Int] orElse 2.right[Int] shouldBe \/-(1)
     "failure".left[Int] orElse 2.right[Int] shouldBe \/-(2)
-  }
 
-  test("fail fast") {
+  test("fail fast"):
     val r = for {
       a <- 1.right[String]
       b <- 0.right[String]
       c <- if (b == 0) "divide.by.zero".left[Int] else (a / b).right[String]
     } yield c
     r shouldBe -\/("divide.by.zero")
-  }
