9b4381fa2b2541106002b63c01d112bb297cf429
objektwerks <objektwerks@runbox.com>
2023-09-26 10:40:18 -0400

    reformatted test

--- src/test/scala/z/EqualityTest.scala
+++ src/test/scala/z/EqualityTest.scala
@@ -4,8 +4,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 import scalaz.Scalaz.*
 
-class EqualityTest extends AnyFunSuite:
-  test("equality") {
+final class EqualityTest extends AnyFunSuite:
+  test("equality"):
     Some(1) === Some(1) assert_=== true
     1.some =/= 2.some assert_=== true
-  }
