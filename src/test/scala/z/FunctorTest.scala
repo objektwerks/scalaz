c4637bb7bf24a9b476320e6631338aea5803701f
objektwerks <objektwerks@runbox.com>
2023-09-26 10:42:33 -0400

    reformatted test

--- src/test/scala/z/FunctorTest.scala
+++ src/test/scala/z/FunctorTest.scala
@@ -5,8 +5,8 @@ import org.scalatest.funsuite.AnyFunSuite
 import scalaz.*
 import scalaz.Scalaz.*
 
-class FunctorTest extends AnyFunSuite:
-  test("map") {
+final class FunctorTest extends AnyFunSuite:
+  test("map"):
     val f1 = (x: Int) => x + 1
     val f2 = (y: Int) => y * 2
     val f3 = f1 map f2
@@ -14,12 +14,10 @@ class FunctorTest extends AnyFunSuite:
 
     assert(Functor[List].map(List(1, 2, 3))(_ * 2) == List(2, 4, 6))
     assert(Functor[Option].map(Some(123))(_.toString) contains "123")
-  }
 
-  test("lift") {
+  test("lift"):
     val f1: Option[Int] => Option[Int] = ((x: Int) => x * 2) lift Monad[Option]
     assert(f1(some(3)) contains 6)
 
     val f2: Option[Int] => Option[Int] = Functor[Option].lift((x: Int) => x * 2)
     assert(f2(Some(3)) contains 6)
-  }
