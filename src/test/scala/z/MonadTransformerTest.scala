00a325f9e4731dc6439135de6a29eb16e4b4417d
objektwerks <objektwerks@runbox.com>
2023-09-26 10:49:21 -0400

    reformatted test

--- src/test/scala/z/MonadTransformerTest.scala
+++ src/test/scala/z/MonadTransformerTest.scala
@@ -5,23 +5,21 @@ import org.scalatest.funsuite.AnyFunSuite
 import scalaz.*
 import scalaz.Scalaz.*
 
-class MonadTransformerTest extends AnyFunSuite:
-  test("list option") {
+final class MonadTransformerTest extends AnyFunSuite:
+  test("list option"):
     type ListOption[A] = OptionT[List, A]
-    val xy = for {
+    val xy = for
       x <- 10.point[ListOption]
       y <- 32.point[ListOption]
-    } yield x + y
+    yield x + y
     xy.getOrElse(0).sum assert_=== 42
-  }
 
-  test("either option") {
+  test("either option"):
     type Error = String
     type ErrorOr[A] = Error \/ A
     type ErrorOptionOr[A] = OptionT[ErrorOr, A]
-    val xy = for {
+    val xy = for
       x <- 10.point[ErrorOptionOr]
       y <- 32.point[ErrorOptionOr]
-    } yield x + y
+    yield x + y
     xy.getOrElse(0).toList.sum assert_=== 42
-  }
