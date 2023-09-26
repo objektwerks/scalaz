78972ea0f675af8d2e17885e999ce575a9e19827
objektwerks <objektwerks@runbox.com>
2023-09-26 10:50:21 -0400

    reformatted test

--- src/test/scala/z/MonoidTest.scala
+++ src/test/scala/z/MonoidTest.scala
@@ -11,31 +11,26 @@ final case class Data(n: Int):
 object Data:
   given Monoid[Data] = Monoid.instance[Data](_ + _, Data(0))
 
-class MonoidTest extends AnyFunSuite:
+final class MonoidTest extends AnyFunSuite:
   def sum[A: Monoid](items: A*): A = items.foldLeft(mzero[A]){ _ |+| _ }
 
-  test("int") {
+  test("int"):
     3 assert_=== Monoid[Int].append(1, 2)
     3 assert_=== 1 |+| 2 |+| mzero[Int]
     6 assert_=== 1 |+| 2 |+| 3
-  }
 
-  test("string") {
+  test("string"):
     "12" assert_=== Monoid[String].append("1", "2")
     "12" assert_=== "1" |+| "2" |+| mzero[String]
-  }
 
-  test("list") {
+  test("list"):
     List(1, 2) assert_=== Monoid[List[Int]].append(List(1), List(2))
     List(1, 2) assert_=== List(1) |+| List(2) |+| mzero[List[Int]]
-  }
 
-  test("case class") {
+  test("case class"):
     assert(sum(Data(1), Data(2), Data(3)) == Data(6))
     assert(sum(List(Data(1), Data(2), Data(3)): _*) == Data(6))
-  }
 
-  test("multiply") {
+  test("multiply"):
     val multiply = Monoid.instance[Int](_ * _, 0)
     6 assert_=== multiply.append(2, 3)
-  }
