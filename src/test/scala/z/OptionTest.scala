9d291ac7f79460e006334545eae02114b76a8670
objektwerks <objektwerks@runbox.com>
2023-09-26 10:51:08 -0400

    reformatted test

--- src/test/scala/z/OptionTest.scala
+++ src/test/scala/z/OptionTest.scala
@@ -4,21 +4,18 @@ import org.scalatest.funsuite.AnyFunSuite
 
 import scalaz.Scalaz.*
 
-class OptionTest extends AnyFunSuite:
-  test("filter") {
+final class OptionTest extends AnyFunSuite:
+  test("filter"):
     some("true") assert_=== (1 < 2).option("true")
     none[String] assert_=== (2 < 1).option("false")
-  }
 
-  test("option") {
+  test("option"):
     1.some assert_=== Some(1)
     none[Int] assert_=== None
-  }
 
-  test("get or else") {
+  test("get or else"):
     1.some? 1 | 0 assert_=== 1
     none? 1 | 0 assert_=== 0
 
     1.some | 2 assert_=== 1
     none | 2 assert_=== 2
-  }
